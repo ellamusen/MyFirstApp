fe890f571111c31d1a8d5af5a764b34d58838a15
ellamusen <dani_1997@live.dk>
2021-03-22 11:39:33 +0100

    Added README.md

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // This is the main activity. It's the entry point for your app.
+    // When you build and run your app, the system launches an instance of this Activity and loads its layout.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
